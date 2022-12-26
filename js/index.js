c6cee9b0ee49e91d6aa9de4cc8efd2254c4d7e11
Matias Cardenes <matiasnahuelcardenes@gmail.com>
2022-12-26 12:41:23 -0300

    preentrega 2

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@ const productos = [
     precio: 130000,
   },
   {
-    id: "Xbox",
+    id: "xbox",
     titulo: "Xbox Series 5",
     imagen: "./img/xboxseries5.jpg",
     categoria: {
@@ -31,7 +31,7 @@ const productos = [
     precio: 200000,
   },
   {
-    id: "Joytick-Xbox",
+    id: "joytick-Xbox",
     titulo: "Joystick Xbox Series 5",
     imagen: "./img/joystickxbox.jpg",
     categoria: {
@@ -41,7 +41,7 @@ const productos = [
     precio: 25000,
   },
   {
-    id: "Joystick-ps4",
+    id: "joystick-ps4",
     titulo: "Joystick PS4",
     imagen: "./img/joystickps4.jpg",
     categoria: {
@@ -51,7 +51,7 @@ const productos = [
     precio: 20000,
   },
   {
-    id: "Jostick-ps5",
+    id: "jostick-ps5",
     titulo: "Joystick PS5",
     imagen: "./img/joystickps5.jpg",
     categoria: {
@@ -61,7 +61,7 @@ const productos = [
     precio: 30000,
   },
   {
-    id: "Iphone-13",
+    id: "iphone-13",
     titulo: "Iphone 13",
     imagen: "./img/iphone13rosa.jpg",
     categoria: {
@@ -71,7 +71,7 @@ const productos = [
     precio: 250000,
   },
   {
-    id: "Iphone-14-plus",
+    id: "iphone-14-plus",
     titulo: "Iphone 14 Plus",
     imagen: "./img/iphone14plus.jpg",
     categoria: {
@@ -81,7 +81,7 @@ const productos = [
     precio: 300000,
   },
   {
-    id: "Iphone-14-pro-max",
+    id: "iphone-14-pro-max",
     titulo: "Iphone 14 Pro Max",
     imagen: "./img/iphone14promax.jpg",
     categoria: {
@@ -91,7 +91,7 @@ const productos = [
     precio: 400000,
   },
   {
-    id: "Airpods-pro",
+    id: "airpods-pro",
     titulo: "Airpods Pro",
     imagen: "./img/airpodspro.webp",
     categoria: {
@@ -101,7 +101,7 @@ const productos = [
     precio: 60000,
   },
   {
-    id: "Airpods-pro-max",
+    id: "airpods-pro-max",
     titulo: "Airpods Pro Max",
     imagen: "./img/airpodspromax.jpg",
     categoria: {
@@ -111,7 +111,7 @@ const productos = [
     precio: 80000,
   },
   {
-    id: "Ipad-pro",
+    id: "ipad-pro",
     titulo: "Ipad Pro",
     imagen: "./img/ipadpro.jpg",
     categoria: {
@@ -121,7 +121,7 @@ const productos = [
     precio: 100000,
   },
   {
-    id: "Apple-watch",
+    id: "apple-watch",
     titulo: "Apple Watch",
     imagen: "./img/applewatch.webp",
     categoria: {
@@ -132,55 +132,99 @@ const productos = [
   },
 ];
 
-const contenedorProductos = document.querySelector("#contenedor-productos");
-const botonesCategorias = document.querySelectorAll(".boton-categoria");
-const tituloPrincipal = document.querySelector("#titulo-principal");
-let botonesAgregar = document.querySelectorAll(".producto-agregar");
+let carrito = []
 
+// const contenedorProductos = document.querySelector("#contenedor-productos");
+// const botonesCategorias = document.querySelectorAll(".boton-categoria");
+// const tituloPrincipal = document.querySelector("#titulo-principal");
+// let botonesAgregar = document.querySelectorAll(".producto-agregar");
 
-function cargarProductos(productosElegidos) {
-  contenedorProductos.innerHTML = "";
+// function cargarProductos(productosElegidos) {
+//   contenedorProductos.innerHTML = "";
 
-  productosElegidos.forEach((producto) => {
-    const div = document.createElement("div");
-    div.classList.add("producto");
-    div.innerHTML = `
-        <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
-        <div class="producto-detalles">
-                <h3 class="producto-titulo">${producto.titulo}</h3>
-                <p class="producto-precio">$${producto.precio}</p>
-                <button class="producto-agregar id="${producto.id}">Agregar</button>
-        </div>
-        `;
+//   productosElegidos.forEach(producto => {
 
-    contenedorProductos.append(div);
-  });
-  nuevosbotonesAgregar();
+//     const div = document.createElement("div");
+//     div.classList.add("producto");
+//     div.innerHTML = `
+//         <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+//         <div class="producto-detalles">
+//                 <h3 class="producto-titulo">${producto.titulo}</h3>
+//                 <p class="producto-precio">$${producto.precio}</p>
+//                 <button class="producto-agregar id="${producto.id}">Agregar</button>
+//         </div>
+//         `;
 
-  console.log(botonesAgregar)
+//     contenedorProductos.append(div);
+//   });
+//   nuevosbotonesAgregar();
+
+// }
+
+// cargarProductos(productos);
+
+// botonesCategorias.forEach(boton => {
+//   boton.addEventListener("click", (e) => {
+//     botonesCategorias.forEach((boton) => boton.classList.remove("active"));
+
+//     e.currentTarget.classList.add("active");
+
+//     if (e.currentTarget.id != "todos") {
+//       const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+//       tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+
+//       const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+//       cargarProductos(productosBoton);
+//     } else {
+//       tituloPrincipal.innerText = "Todos los productos";
+//       cargarProductos(productos);
+//     }
+//   });
+// });
+
+// function nuevosbotonesAgregar(){
+//   botonesAgregar = document.querySelectorAll(".producto-agregar")
+
+//   botonesAgregar.forEach(boton => {
+//     boton.addEventListener("click", agregarAlCarrito)
+//   });
+// }
+
+// const productosEnCarrito = []
+
+// function agregarAlCarrito(e){
+
+//   const idBoton = e.currentTarget.id;
+//   const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+//   productosEnCarrito.push(productoAgregado);
+//   console.log(productosEnCarrito);
+
+// }
+
+function findProduct (arr, filtro){
+  const encontrado = arr.find((product)=>{
+    return product.id.includes(filtro.toLowerCase())
+  })
+  return encontrado;
 }
 
-cargarProductos(productos);
+let buscar = prompt("Ingresa el nombre del producto de consola o telefonico que desee consultar");
 
-botonesCategorias.forEach(boton => {
-  boton.addEventListener("click", (e) => {
-    botonesCategorias.forEach((boton) => boton.classList.remove("active"));
+const prodEncontrado = findProduct(productos, buscar);
 
-    e.currentTarget.classList.add("active");
+alert(prodEncontrado.titulo + " $" + prodEncontrado.precio);
 
-    if (e.currentTarget.id != "todos") {
-      const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id); 
-      tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+seleccion = prompt ("Desea consultar sobre otro producto?");
 
-      const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
-      cargarProductos(productosBoton);
-    } else {
-      tituloPrincipal.innerText = "Todos los productos";
-      cargarProductos(productos);
-    }
-  });
-});
 
-function nuevosbotonesAgregar(){
-  botonesAgregar = document.querySelectorAll(".producto-agregar")
+while(seleccion === "si"){
+  let buscar = prompt("Ingresa el nombre del producto de consola o telefonico que desee consultar");
+  const prodEncontrado = findProduct(productos, buscar);
+  alert(prodEncontrado.titulo + " $" + prodEncontrado.precio);
+  seleccion = prompt ("Desea consultar sobre otro producto?");
 }
+
+if(seleccion === "no"){
+  alert ("Gracias por su consulta!")
+};
